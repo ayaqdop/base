1c562ec8d398bbd377b8ed3cd77a911ceabf805a
Куаныш Идрисов < >
2017-01-29 01:06:49 +0600

    Added moves_left test

--- player.py
+++ player.py
@@ -1,4 +1,8 @@
-class Player:
+class Player(object):
+
+    MOVES_LEFT = 3
+
     def __init__(self, team, number):
         self.team = team
         self.number = number
+        self.moves_left = Player.MOVES_LEFT
--- tests/player_tests.py
+++ tests/player_tests.py
@@ -14,6 +14,7 @@ class PlayerTest(unittest.TestCase):
 
         self.assertEqual(team, target.team)
         self.assertEqual(number, target.number)
+        self.assertEqual(Player.MOVES_LEFT, target.moves_left)
 
     def test_init_false(self):
         team = "Test Team"
@@ -23,6 +24,16 @@ class PlayerTest(unittest.TestCase):
         self.assertEqual("Test Team", target.team)
         self.assertNotEqual("42", target.number)
 
+    def test_moves_left(self):
+        target = Player("Test Team", 42)
+        self.assertEqual(Player.MOVES_LEFT, target.moves_left)
+
+        target.moves_left -= 1
+        self.assertEqual(2, target.moves_left)
+
+        target.moves_left = 5
+        self.assertEqual(5, target.moves_left)
+        
 
 if __name__ == '__main__':
     unittest.main()
