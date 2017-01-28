47df36c0419ad50e393f13afee208242387aa9a3
quanysh <kuanysh.idrissov@gmail.com>
2017-01-29 02:10:44 +0600

    Added Team tests

--- team.py
+++ team.py
@@ -1,9 +1,10 @@
 class Team(object):
-    
+
     MOVES_LEFT = 5
-    
+
     def __init__(self, name, formation, players):
         self.name = name
         self.formation = formation
         self.players = players
         self.score = 0
+        self.moves_left = Team.MOVES_LEFT
---
+++ tests/team_tests.py
@@ -0,0 +1,25 @@
+import unittest
+import sys
+
+sys.path.insert(0, "/home/ayaqdop/base")
+
+from team import Team
+
+class TeamTest(unittest.TestCase):
+
+    def test_init(self):
+        name = "Real Madrid"
+        formation = "4-4-2"
+        players = []
+        target = Team(name, formation, players)
+
+        self.assertEqual(name, target.name)
+        self.assertEqual(formation, target.formation)
+        self.assertEqual(players, target.players)
+
+    def test_score(self):
+        target = Team("Barcelona", "3-5-2", [])
+        self.assertEqual(0, target.score)
+
+        target.score += 2
+        self.assertEqual(2, target.score)
