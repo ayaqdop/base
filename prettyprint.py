57c4007e1858e5c455a5d8857bb9102d3b168b55
pheeria <olzhas.askar@gmail.com>
2017-02-10 23:43:25 +0100

    Implemented PrettyPrint.colored_print()

--- prettyprint.py
+++ prettyprint.py
@@ -1,4 +1,20 @@
+import sys
+
+BLACK = "\033[1;30m"
+RED = "\033[1;31m"
+GREEN = "\033[1;32m"
+YELLOW = "\033[1;33m"
+BLUE = "\033[1;34m"
+MAGENTA = "\033[1;35m"
+CYAN = "\033[1;36m"
+WHITE = "\033[1;37m"
+
+RESET = "\033[0;0m"
+
 class PrettyPrint:
 
-    def colored_print(self, text, color):
-        pass
+
+    def colored_print(self, text, color, end=''):
+        sys.stdout.write(color)
+        print(text, end=end)
+        sys.stdout.write(RESET)
--- tests/prettyprint_tests.py
+++ tests/prettyprint_tests.py
@@ -3,11 +3,17 @@ import sys
 
 sys.path.insert(0, "home/ayaqdop/base")
 
-from prettyprint import PrettyPrint
+import prettyprint
 
 class PrettyPrintTest(unittest.TestCase):
 
     def test_colored_print(self):
-        target = PrettyPrint()
+        self.assertEqual("\033[1;30m", prettyprint.BLACK)
+        self.assertEqual("\033[1;31m", prettyprint.RED)
+        self.assertEqual("\033[1;32m", prettyprint.GREEN)
+        self.assertEqual("\033[1;33m", prettyprint.YELLOW)
+        self.assertEqual("\033[1;34m", prettyprint.BLUE)
+        self.assertEqual("\033[1;35m", prettyprint.MAGENTA)
+        self.assertEqual("\033[1;36m", prettyprint.CYAN)
+        self.assertEqual("\033[1;37m", prettyprint.WHITE)
 
-        target.colored_print("Text", "RED")
