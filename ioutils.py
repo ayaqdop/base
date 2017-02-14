0e87562289a9c9e073d26b077a3948ce84cf08f2
quanysh <kuanysh.idrissov@gmail.com>
2017-02-15 00:13:16 +0600

    Input Parser added

--- ioutils.py
+++ ioutils.py
@@ -1,11 +1,28 @@
 import re
+import string
 
 def input_parser():
-    input_value = input("Enter positions: ")
-    player_move = re.split('\W+',input_value)
-        
+    input_value = re.split('\W+',input("Enter positions: "))
+    
+    if(input_value[0] == input_value[1] 
+        or not input_value[0][0] in 'abcdefghijklmnopABCDEFGHIJKLMNOP' 
+        or not input_value[1][0] in 'abcdefghijklmnopABCDEFGHIJKLMNOP'
+        or input_value[0][1:].isalpha()
+        or input_value[1][1:].isalpha()
+        or len(input_value[0])>3
+        or len(input_value[1])>3
+        or not int(input_value[0][1:]) >=0
+        or not int(input_value[0][1:]) <=25
+        or not int(input_value[1][1:]) >=0
+        or not int(input_value[1][1:]) <=25):
+        print("Something wrong with your values, please retype your player and move position: ")
+        input_parser()
 
-    print (player_move)
-        
+    else:
+        input_value[0] = input_value[0][0].lower()+input_value[0][1:]
+        input_value[1] = input_value[1][0].lower()+input_value[1][1:]
+        print(input_value[0], input_value[1])
+
+
 
 input_parser()
