9bc20b2123e8e3e8ddda9b131c6ff6a4334418c1
pheeria <olzhas.askar@gmail.com>
2017-02-19 18:35:24 +0100

    Fixed test

--- tests/console_tests.py
+++ tests/console_tests.py
@@ -8,12 +8,12 @@ import console
 class ConsoleTest(unittest.TestCase):
 
     def test_colored_print(self):
-        self.assertEqual("\033[1;30m", prettyprint.BLACK)
-        self.assertEqual("\033[1;31m", prettyprint.RED)
-        self.assertEqual("\033[1;32m", prettyprint.GREEN)
-        self.assertEqual("\033[1;33m", prettyprint.YELLOW)
-        self.assertEqual("\033[1;34m", prettyprint.BLUE)
-        self.assertEqual("\033[1;35m", prettyprint.MAGENTA)
-        self.assertEqual("\033[1;36m", prettyprint.CYAN)
-        self.assertEqual("\033[1;37m", prettyprint.WHITE)
+        self.assertEqual("\033[1;30m", console.BLACK)
+        self.assertEqual("\033[1;31m", console.RED)
+        self.assertEqual("\033[1;32m", console.GREEN)
+        self.assertEqual("\033[1;33m", console.YELLOW)
+        self.assertEqual("\033[1;34m", console.BLUE)
+        self.assertEqual("\033[1;35m", console.MAGENTA)
+        self.assertEqual("\033[1;36m", console.CYAN)
+        self.assertEqual("\033[1;37m", console.WHITE)
 
