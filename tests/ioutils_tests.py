5c49c09c6e3aa73516f01ab0a6ab0579c98e06ee
pheeria <olzhas.askar@gmail.com>
2017-02-24 09:58:16 +0100

    Added failing conditions

--- tests/ioutils_tests.py
+++ tests/ioutils_tests.py
@@ -1,5 +1,6 @@
 import unittest
 import sys
+import string
 
 sys.path.insert(0, "/home/ayaqdop/base")
 
@@ -10,15 +11,26 @@ class IOTest(unittest.TestCase):
     def test_init(self):
         pass
 
-    def test_is_valid_letter_true(self):
+    def test_is_valid_letter_single_letters(self):
         target = IOUtils()
-        self.assertTrue(target.is_valid_letter('a'))
-        self.assertTrue(target.is_valid_letter('A'))
+
+        for c in string.ascii_lowercase:
+            if c < 'q':
+                self.assertTrue(target.is_valid_letter(c))
+            else:
+                self.assertFalse(target.is_valid_letter(c))
+
+        for c in string.ascii_uppercase:
+            if c < 'Q':
+                self.assertTrue(target.is_valid_letter(c))
+            else:
+                self.assertFalse(target.is_valid_letter(c))
 
     def test_is_valid_letter_false(self):
         target = IOUtils()
-        self.assertFalse(target.is_valid_letter('w'))
-        self.assertFalse(target.is_valid_letter('W'))
+        self.assertFalse(target.is_valid_letter("False"))
+        self.assertFalse(target.is_valid_letter("false"))
+        self.assertFalse(target.is_valid_letter(42))
 
     def test_is_not_same_pos_true(self):
         target = IOUtils()
