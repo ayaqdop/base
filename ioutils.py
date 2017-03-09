05bcd405a7c515ecda4aac4eaa0e6e08d660e885
pheeria <olzhas.askar@gmail.com>
2017-03-09 21:39:34 +0100

    Fixed indent error and deleted comments

--- ioutils.py
+++ ioutils.py
@@ -6,41 +6,23 @@ class IOUtils:
 
 
     def input_parser(self, input_value):
-        input_value = re.split('\W+',input("Enter positions: "))     
         current_pos = input_value[0]
         next_pos = input_value[1]
         step = {}
         step['FROM']={}
         step['TO']={}
-        if(is_valid_position(current_pos) and is_valid_position(next_pos) and is_not_same_position(current_pos,next_pos)):
+        if (self.is_valid_position(current_pos)
+            and self.is_valid_position(next_pos)
+            and self.is_not_same_position(current_pos,next_pos)):
             step['FROM']['X']=convert_letter(current_pos[0])
             step['FROM']['Y']=current_pos[1:]
             step['TO']['X']==convert_letter(next_pos[0])
             step['TO']['Y']==next_pos[1:]
         return step
-'''        input_value = re.split('\W+',input("Enter positions: "))
-        
-        if(input_value[0] == input_value[1] 
-            or not input_value[0][0] in 'abcdefghijklmnopABCDEFGHIJKLMNOP' 
-            or not input_value[1][0] in 'abcdefghijklmnopABCDEFGHIJKLMNOP'
-            or input_value[0][1:].isalpha()
-            or input_value[1][1:].isalpha()
-            or len(input_value[0])>3
-            or len(input_value[1])>3
-            or not int(input_value[0][1:]) >=0
-            or not int(input_value[0][1:]) <=25
-            or not int(input_value[1][1:]) >=0
-            or not int(input_value[1][1:]) <=25):
-            print("Something wrong with your values, please retype your player and move position: ")
-            input_parser()
-        else:
-            input_value[0] = input_value[0][0].lower()+input_value[0][1:]
-            input_value[1] = input_value[1][0].lower()+input_value[1][1:]
-            print(input_value[0], input_value[1])
-'''         #current pos = {curX:input_value[0][0], curY:input_value[0][1:]}
+
     def is_not_same_position(self,pos1,pos2):
         return pos1 != pos2
-            
+
     def is_valid_letter(self, letter):
         return len(letter)==1 and letter.isalpha() and "a" <= letter <= "p"
 
