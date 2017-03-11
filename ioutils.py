9d44ecd3feeb48c52b804a93d196ca9ff0d56758
pheeria <olzhas.askar@gmail.com>
2017-03-11 19:36:09 +0100

    Changed code style

--- ioutils.py
+++ ioutils.py
@@ -5,27 +5,31 @@ import string
 class IOUtils:
 
     def input_parser(self, input_value):
-        input_value = re.split('\W+',input_value)
-        current_pos = input_value[0]
-        next_pos = input_value[1]
-        if (self.is_valid_position(current_pos)
-            and self.is_valid_position(next_pos)
-            and self.is_not_same_position(current_pos,next_pos)):
-            step={
-            'FROM':{'X': self.convert_letter(current_pos[0]), 
-            'Y': int(current_pos[1:])},
-            'TO':{'X': self.convert_letter(next_pos[0]), 
-            'Y': int(next_pos[1:])}
+        input_value = re.split("\W+",input_value)
+        current_position = input_value[0]
+        next_position = input_value[1]
+        if (self.is_valid_position(current_position)
+            and self.is_valid_position(next_position)
+            and self.is_not_same_position(current_position,next_position)):
+            step = {
+                "FROM" : {
+                    "X": self.convert_letter(current_position[0]),
+                    "Y": int(current_position[1:])
+                },
+                "TO": {
+                    "X": self.convert_letter(next_position[0]),
+                    "Y": int(next_position[1:])
+                }
             }
             return step
         else:
-            return "Incorrect input, please retype your step"
+            return None
 
-    def is_not_same_position(self,pos1,pos2):
-        return pos1 != pos2
+    def is_not_same_position(self, position1, position2):
+        return position1 != position2
 
     def is_valid_letter(self, letter):
-        return len(letter)==1 and letter.isalpha() and "a" <= letter <= "p"
+        return len(letter) == 1 and letter.isalpha() and "a" <= letter <= "p"
 
     def is_valid_number(self, number):
         return number.isnumeric() and 0 <= int(number) <= 25
