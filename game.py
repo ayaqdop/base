8fa44b85e6203a01680dc3bbca75966bd7011c2d
quanysh <kuanysh.idrissov@gmail.com>
2018-02-08 00:04:29 +0600

    Just add variables and function

--- game.py
+++ game.py
@@ -4,10 +4,13 @@ from field import Field
 
 class Game:
 
-    def __init__(self):
+    def __init__(self, player1, player2, team1, team2):
         self.time_limit = 0
         self.score_limit = 0
         self.turn = None
         self.ball = Ball()
         self.teams = None
         self.field = Field()
+
+    def move(self, move_from, move_to):
+        pass
